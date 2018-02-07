2935254cbbcdec49511713d64e7b4b8c41464faa
Antonin Januska <antonin@antjanus.com>
2018-02-06 18:58:30 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  onSearchUpdate() {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <h1>Wikipedia Viewer</h1>
+        <p>
+          <input type="text" placeholder="Search wikipedia" />
         </p>
       </div>
     );
