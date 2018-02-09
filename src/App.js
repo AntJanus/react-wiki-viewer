a69f55ea3890f19f1d0764b6aed210ceb14450f6
antjanus <antonin@antjanus.com>
2018-02-08 19:13:48 -0700

    feat(app): Wikipedia random article link'

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const searchURL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+
 class App extends Component {
-  onSearchUpdate() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: '',
+      searchResults: []
+    };
+
+    this.onSearchUpdate = this.onSearchUpdate.bind(this);
+  }
+
+  onSearchUpdate(event) {
+    this.setState({
+      searchValue: event.target.value
+    });
+  }
+
+  onSearch() {
 
   }
 
@@ -11,8 +29,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Wikipedia Viewer</h1>
+        <a href="https://en.wikipedia.org/wiki/Special:Random">Random article</a>
         <p>
-          <input type="text" placeholder="Search wikipedia" />
+          <input type="text" placeholder="Search wikipedia" value={this.state.searchValue} onChange={onSearchUpdate}/>
         </p>
       </div>
     );
